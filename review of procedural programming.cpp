3d00e2c84ae7593c7b301f4ba9a2014164de1d78
careybarberjr@gmail.com <careybarberjr@gmail.com>
2025-02-11 13:18:27 -0500

    z

--- review of procedural programming.cpp
+++ review of procedural programming.cpp
@@ -2,10 +2,40 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+using namespace std; 
 
-double 
+int calculateaverage(int, int, int);
 
 int main()
 {
+ 
+    ifstream inputFile;
+    inputFile.open("StudentGrades.txt");
+    if (inputFile.fail())
+    {
+        cout << "could not open file, rerun the program and try again.";
+        return 0;
+    }
+
+    const int GradeAmt = 5;
+    int grade= 0;
+    int arr[GradeAmt];
+    int currentgrades = 0;
     
+    void addElement(int arr[], int& currentgrades, int element, int GradeAmt);
+    {
+        while (currentgrades < GradeAmt)
+        {
+            arr[currentgrades++] = grade;
+        }
+
+    }
+}
+
+int calculateaverage(int scores, int combined, int average)
+{
+
+    return average;
 }
