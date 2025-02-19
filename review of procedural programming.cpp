eb8f753e968fb4ee64f6c6a0fa9d67bace6a2fbc
careybarberjr@gmail.com <careybarberjr@gmail.com>
2025-02-19 15:42:46 -0500

    a

--- review of procedural programming.cpp
+++ review of procedural programming.cpp
@@ -1,61 +1,45 @@
-// review of procedural programming.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <random>
 using namespace std;
 
-int PlayerInput(string playerinput, int playerinputstandin = 0);
+int PlayerInput(string playerinput, int playerinputstandin);
+void match(int playerinputstandin, int robotscore, int playerscore);
+void roundwinner(int playerscore, int robotscore);
+void matchwinner(int playerwins, int robotwins);
 
 int main()
 {
     random_device generator;
     uniform_int_distribution<int> robochoice(1, 3);
     string playerinput;
-    int playerinputstandin = 0;
-    
-        
-        playerinputstandin =PlayerInput( playerinput, playerinputstandin);
+    int playerwins = 0;
+    int robotwins= 0;
+    int rounds = 0;
+    int matches = 0;
+    int roundsaskedfor = 0;
+    int matchesaskedfor = 0;
+   
+    cout << "How many rounds would you like to play?" << endl;
+    cin >> roundsaskedfor;
+    cout << "How many matches would you like to play?" << endl;
+    cin >> matchesaskedfor;
 
-      
-
-       
-        int robotchoice = robochoice(generator);
-
-        if (playerinputstandin == robotchoice)
+    while (matches < matchesaskedfor)
+    {
+        rounds = 0;
+        int playerscore = 0;
+        int robotscore = 0;
+        while (rounds < roundsaskedfor)
         {
-            cout << "its a draw!" << endl;
+            int playerinputstandin = 0;
+            playerinputstandin = PlayerInput(playerinput, playerinputstandin);
+            match(playerinputstandin, robotscore, playerscore);
+            rounds++;
         }
-        else if (playerinputstandin == 1 && robotchoice == 2)
-        {
-            cout << "you lose! paper wraps rock.";
-                return 0;
-        }
-        else if (playerinputstandin == 1 && robotchoice == 3)
-        {
-            cout << "you win! rock smashes scissors.";
-                return 0;
-        }
-        else if (playerinputstandin == 2 && robotchoice == 1)
-        {
-            cout << "you win! paper wraps rock.";
-                return 0;
-        }
-        else if (playerinputstandin == 2 && robotchoice == 3)
-        {
-            cout << "you lose! scissors cuts paper.";
-                return 0;
-        }
-        else if (playerinputstandin == 3 && robotchoice == 2)
-        {
-            cout << "you win! scissors cuts paper.";
-                return 0;
-        }
-        else if (playerinputstandin == 3 && robotchoice == 1)
-        {
-            cout << "you lose! rock smashes scissors.";
-                return 0;
-        }
+        roundwinner(playerscore, robotscore);
+        matches++;
+    }
+    matchwinner(playerwins, robotwins);
 
      
 }
@@ -84,4 +68,90 @@ int PlayerInput(string playerinput, int playerinputstandin)
     }
     
     return  playerinputstandin;
+}
+void match(int playerinputstandin, int robotscore, int playerscore)
+{
+    random_device generator;
+    uniform_int_distribution<int> robochoice(1, 3);
+    int robotchoice = robochoice(generator);
+    int loopvar = 0;
+    while (loopvar == 0)
+    {
+        if (playerinputstandin == robotchoice)
+        {
+            cout << "its a draw!" << endl;
+            loopvar++;
+        }
+        else if (playerinputstandin == 1 && robotchoice == 2)
+        {
+            cout << "you lose! paper wraps rock."<<endl;        
+            robotscore + 1;
+            loopvar++;
+           
+        }
+        else if (playerinputstandin == 1 && robotchoice == 3)
+        {
+            cout << "you win! rock smashes scissors." << endl;
+            playerscore + 1;
+            loopvar++;
+        }
+        else if (playerinputstandin == 2 && robotchoice == 1)
+        {
+            cout << "you win! paper wraps rock." << endl;
+            playerscore + 1;
+            loopvar++;
+        }
+        else if (playerinputstandin == 2 && robotchoice == 3)
+        {
+            cout << "you lose! scissors cuts paper." << endl;
+            robotscore + 1;
+            loopvar++;
+        }
+        else if (playerinputstandin == 3 && robotchoice == 2)
+        {
+            cout << "you win! scissors cuts paper." << endl;
+            playerscore + 1;
+            loopvar++;
+        }
+        else if (playerinputstandin == 3 && robotchoice == 1)
+        {
+            cout << "you lose! rock smashes scissors." << endl;
+            robotscore + 1;
+            loopvar++;
+
+        }
+    }
+}
+
+void roundwinner(int playerscore, int robotscore)
+{
+    if (playerscore == robotscore)
+    {
+        cout << "this match is a draw" << endl;
+    }
+    else if (playerscore > robotscore)
+    {
+        cout << "you win this match" << endl;
+        playerscore++;
+    }
+    else
+    {
+        cout << "you lost this match" << endl;
+        robotscore++;
+    }
+}
+void matchwinner(int playerwins, int robotwins)
+{
+    if (playerwins == robotwins)
+    {
+        cout << "this tournament is a draw" << endl;
+    }
+    else if (playerwins > robotwins)
+    {
+        cout << "you won the tournament" << endl;
+    }
+    else
+    {
+        cout << "you lost the tournament" << endl;
+    }
 }
